4bba5e9893ed4c68557bf2abe324fef474e56eca
Kacper Smyczyk <kacpersmyczyk@gmail.com>
2024-12-21 20:36:10 +0100

    config: add image domain to next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["eu-west-2.graphassets.com"],
+  },
 };
 
 export default nextConfig;
