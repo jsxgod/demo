86b20eae1640314e705350b0805088279c5762d3
Kacper Smyczyk <kacpersmyczyk@gmail.com>
2024-12-22 02:04:06 +0100

    feat: use custom gradient in the CardBackground component

--- src/1_Components/Cards/CardBackground/CardBackground.tsx
+++ src/1_Components/Cards/CardBackground/CardBackground.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
 
 import S from "./CardBackground.module.scss";
+import { CmsComponentGradient } from "@/types/cms/Misc.types";
 
 export interface CardBackgroundProps {
   isReversed: boolean;
   size: number;
+  gradient?: CmsComponentGradient;
   paddingSize?: number;
 }
 
 const CardBackground: FC<CardBackgroundProps> = ({
   isReversed,
   size = 0,
+  gradient,
   paddingSize = 72,
 }) => {
   return (
@@ -21,6 +24,17 @@ const CardBackground: FC<CardBackgroundProps> = ({
         ...(isReversed
           ? { left: "unset", right: `-${paddingSize}px` }
           : { left: `-${paddingSize}px`, right: "unset" }),
+        background: gradient
+          ? `linear-gradient(
+            ${gradient?.angle}deg,
+            ${gradient?.firstColor.hex} 0%,
+            ${gradient?.secondColor.hex} 90%
+          )`
+          : `linear-gradient(
+            130deg,
+            #DAD3B7 0%,
+            rgba(255, 255, 255, 1) 90%
+          )`,
       }}
     />
   );
